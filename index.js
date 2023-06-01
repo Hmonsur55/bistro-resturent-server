d875cbd667d7566724e275198c06766bd7596ac7
Hmonsur55 <116294687+Hmonsur55@users.noreply.github.com>
2023-06-02 01:22:12 +0600

    add deleted method on server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // meddlewear 
 app.use(cors())
 app.use(express.json()) 
@@ -38,13 +38,29 @@ async function run() {
           res.send(result)
       })
 
-    // cart
+    // cart collection 
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email
+      if (!email) {
+        res.send ([])
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray()
+      res.send(result)
+    })
+
     app.post('/carts', async (req, res) => {
       const item = req.body;
       console.log(item)
       const result = await cartCollection.insertOne(item)
       res.send(result)
-  })
+    })
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
